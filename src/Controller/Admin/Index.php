51fdf7a53c6eaa28836f4337b48d6237061451e3
Jim Schmid <jim.schmid@1up.io>
2023-05-02 17:55:37 +0200

    Since there is no data for the dashboard, redirect for now

--- src/Controller/Admin/Index.php
+++ src/Controller/Admin/Index.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(path: '/admin', name: 'admin.index')]
 class Index extends AbstractController
 {
-    public function __invoke(): Response
+    public function __invoke(): RedirectResponse
     {
-        return $this->render('/admin/index.html.twig');
+        return $this->redirectToRoute('admin.admin.index');
     }
 }
