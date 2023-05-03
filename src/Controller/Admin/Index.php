b4d93a1d3e000b6e1f2ee854eb877537f5a4e6be
Jim Schmid <jim.schmid@1up.io>
2023-05-03 08:23:15 +0200

    Updated dependencies and fixed pipeline

--- src/Controller/Admin/Index.php
+++ src/Controller/Admin/Index.php
@@ -6,7 +6,6 @@ namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(path: '/admin', name: 'admin.index')]
