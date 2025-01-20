1f960a48d4e0a2e7c0d8a5a524fcf9e1bf3e07d4
Michael Blättler <michi.blaettler@bluewin.ch>
2025-01-20 16:10:51 +0100

    style: apply style used in the information documents for parents
    There are several documents created for teachers, students and parents. To make sure
    the project is recognized once the app is opened, the colors of the app should
    be adjusted accordingly.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,13 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+                sans: ['Karla', 'Inter', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'zebrelina-light-blue': '#C1E3E9',
+                'zebrelina-dark-blue':  '#79B3C5',
+                'zebrelina-light-yellow':  '#F8F2D3',
+            }
         },
     },
     plugins: [
